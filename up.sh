4af9821b980949427b8d9e9431234d80a3177cc4
Gonéri Le Bouder <goneri@lebouder.net>
2023-02-09 17:31:57 -0500

    set -eux

--- up.sh
+++ up.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eux
 ansible-galaxy collection install --requirements-file requirements.yml --collections-path collections
 ansible-playbook deploy.yaml -e @$USER.yaml
 ansible-playbook extra.yaml -i gpu,
