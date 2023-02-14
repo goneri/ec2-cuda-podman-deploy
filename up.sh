be12d8a112efd2446b772cdccc4926f11d3d3303
Gonéri Le Bouder <goneri@lebouder.net>
2023-02-14 13:10:49 -0500

    sync the ansible-wisdom-model repository

--- up.sh
+++ up.sh
@@ -2,4 +2,4 @@
 set -eux
 ansible-galaxy collection install --requirements-file requirements.yml --collections-path collections
 ansible-playbook deploy.yaml -e @$USER.yaml
-ansible-playbook extra.yaml -i gpu,
+ansible-playbook extra.yaml -i gpu, -e @$USER.yaml
