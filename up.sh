9b437823fa3573a6dfdce06d5618d2f2edc6fa74
Gonéri Le Bouder <goneri@lebouder.net>
2023-03-28 13:38:26 -0400

    deploy different models in parallel

--- up.sh
+++ up.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -eux
 ansible-galaxy collection install --requirements-file requirements.yml --collections-path collections
+pip install -r requirements.txt
+find collections/ansible_collections/ -maxdepth 3 -name requirements.txt -exec pip install -r {} \;
 ansible-playbook deploy.yaml -e @$USER.yaml
 ansible-playbook extra.yaml -i gpu, -e @$USER.yaml
