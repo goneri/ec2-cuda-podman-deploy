b0aaac94dd2b8d874fc166892c2be9b545a08ca3
Gonéri Le Bouder <goneri@lebouder.net>
2024-07-30 08:15:40 -0400

    also install the roles

--- up.sh
+++ up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eux
 ansible-galaxy collection install --requirements-file requirements.yml --collections-path collections
+ansible-galaxy role install -r requirements.yml --roles-path roles
 pip install -r requirements.txt
 find collections/ansible_collections/ -maxdepth 3 -name requirements.txt -exec pip install -r {} \;
 ansible-playbook deploy.yaml -e @$USER.yaml
