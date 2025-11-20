a69f9cb9a09c682568c5a0a83d78cf4532ba083f
Gonéri Le Bouder <goneri@lebouder.net>
2025-11-20 15:20:37 -0500

    remove the nvidia role and various improvements

--- down.sh
+++ down.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+source venv/bin/activate
+test -d aws-automation || git clone https://gitlab.cee.redhat.com/compute/aws-automation
+python3 ./aws-automation/aws-saml.py --target-account 354468940538 --target-role 354468940538-poweruser
+
+export AWS_PROFILE=saml
+export AWS_REGION=ca-central-1
+
 ansible-playbook purge.yaml -e @$USER.yaml
--- up.sh
+++ up.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -eux
+
+
 source venv/bin/activate
+test -d aws-automation || git clone https://gitlab.cee.redhat.com/compute/aws-automation
+python3 ./aws-automation/aws-saml.py --target-account 354468940538 --target-role 354468940538-poweruser
+
+export AWS_PROFILE=saml
+export AWS_REGION=ca-central-1
+
 pip install -r requirements.txt
 ansible-galaxy collection install --requirements-file requirements.yml --collections-path collections
 ansible-galaxy role install -r requirements.yml --roles-path roles
 pip install -r requirements.txt
 find collections/ansible_collections/ -maxdepth 3 -name requirements.txt -exec pip install -r {} \;
+
+test -f $USER.yaml || cp -v defaults.yaml $USER.yaml
 ansible-playbook deploy.yaml -e @$USER.yaml
